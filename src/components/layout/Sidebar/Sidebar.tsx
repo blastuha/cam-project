4f482e5321bb473d934506fa54307ef3bfbfbc1c
Boris <shevnin.boris2@yandex.ru>
2024-07-15 18:06:53 +0300

    изменил стили у SidebarMenu

--- src/components/layout/Sidebar/Sidebar.tsx
+++ src/components/layout/Sidebar/Sidebar.tsx
@@ -2,57 +2,15 @@ import React from 'react';
 import Divider from '@mui/material/Divider';
 import styles from './Sidebar.module.scss';
 import { SidebarMenu } from './SidebarMenu/SidebarMenu';
+import { CompanyIcon } from '@components/icons';
 
 export const Sidebar = () => {
   return (
     <aside>
       <header className={styles.sidebarHeader}>
         <div className={styles.logo}>
-          <svg
-            className=" icon-30"
-            viewBox="0 0 30 30"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <rect
-              x="-0.757324"
-              y="19.2427"
-              width="28"
-              height="4"
-              rx="2"
-              transform="rotate(-45 -0.757324 19.2427)"
-              fill="#3a57e8"
-            ></rect>
-            <rect
-              x="7.72803"
-              y="27.728"
-              width="28"
-              height="4"
-              rx="2"
-              transform="rotate(-45 7.72803 27.728)"
-              fill="#3a57e8"
-            ></rect>
-            <rect
-              x="10.5366"
-              y="16.3945"
-              width="16"
-              height="4"
-              rx="2"
-              transform="rotate(45 10.5366 16.3945)"
-              fill="#3a57e8"
-            ></rect>
-            <rect
-              x="10.5562"
-              y="-0.556152"
-              width="28"
-              height="4"
-              rx="2"
-              transform="rotate(45 10.5562 -0.556152)"
-              fill="#3a57e8"
-            ></rect>
-          </svg>
+          <CompanyIcon />
         </div>
-
         <h1>Company Name</h1>
       </header>
 
