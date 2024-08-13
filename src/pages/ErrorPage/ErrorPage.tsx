3861a7edfcb197a6ae6ad250acd6aad0f08bf35f
Boris <shevnin.boris2@yandex.ru>
2024-08-13 18:57:49 +0300

    Изменил текст в ErrorPage

--- src/pages/ErrorPage/ErrorPage.tsx
+++ src/pages/ErrorPage/ErrorPage.tsx
@@ -4,12 +4,16 @@ import styles from './ErrorPage.module.scss';
 export const ErrorPage = () => {
   return (
     <div className={styles.gradient}>
-      <div className={styles.container}>Error</div>
+      <div className={styles.container}>
+        <p>Упс! Эта страница не найдена.</p>
+        <p>Запрошенная страница не существует.</p>
+        <button>На домашнюю страницу</button>
+      </div>
       <div className={styles.box}>
-        <div className={styles.circle_s}>
-          <div className={styles.circle_m}>
-            <div className={styles.circle_l}>
-              <div className={styles.circle_xl}>
+        <div className={styles.circle_xl}>
+          <div className={styles.circle_l}>
+            <div className={styles.circle_m}>
+              <div className={styles.circle_s}>
                 <div className={styles.circle_xs}></div>
               </div>
             </div>
