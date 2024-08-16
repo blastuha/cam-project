15d83e7e294e83d45f013c905fab9e616e719786
Boris <shevnin.boris2@yandex.ru>
2024-08-16 10:47:55 +0300

    Отобразил таблицей все данные в AllVideosPage

--- src/pages/AllVideosPage/AllVideosPage.tsx
+++ src/pages/AllVideosPage/AllVideosPage.tsx
@@ -4,6 +4,16 @@ import { PageContainer } from '@components/containers/PageContainer/PageContaine
 import { useQuery } from '@tanstack/react-query';
 import { getAllVideos } from '@api/analyzer/getAllVideos';
 import { useNavigate } from 'react-router-dom';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  CircularProgress,
+} from '@mui/material';
 
 export const AllVideosPage = () => {
   const { data: allVideosData, isLoading } = useQuery({
@@ -13,20 +23,53 @@ export const AllVideosPage = () => {
 
   const navigate = useNavigate();
 
-  console.log('allVideosData', allVideosData);
+  if (isLoading) {
+    return (
+      <PageContainer>
+        <CircularProgress />
+      </PageContainer>
+    );
+  }
 
   return (
     <PageContainer>
-      {allVideosData?.data?.map((video) => (
-        <p onClick={() => navigate('/video/' + video.id)}>{video.filename}</p>
-      ))}
-      {/* <span onClick={() => navigate('/video/' + allVideosData?.data[0].id)}>
-        {allVideosData?.data[0] && allVideosData?.data[0].filename}
-      </span>
-
-      <p onClick={() => navigate('/video/' + allVideosData?.data[1].id)}>
-        {allVideosData?.data[1] && allVideosData?.data[1].filename}
-      </p> */}
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 650 }} aria-label="videos table">
+          <TableHead>
+            <TableRow>
+              <TableCell>Filename</TableCell>
+              <TableCell align="right">Description</TableCell>
+              <TableCell align="right">Status</TableCell>
+              <TableCell align="right">Start Date</TableCell>
+              <TableCell align="right">End Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {allVideosData?.data?.map((video) => (
+              <TableRow
+                key={video.id}
+                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                onClick={() => navigate('/video/' + video.id)}
+                style={{ cursor: 'pointer' }}
+              >
+                <TableCell component="th" scope="row">
+                  {video.filename}
+                </TableCell>
+                <TableCell align="right">
+                  {video.description || 'No Description'}
+                </TableCell>
+                <TableCell align="right">{video.status}</TableCell>
+                <TableCell align="right">
+                  {new Date(video.date_start).toLocaleString()}
+                </TableCell>
+                <TableCell align="right">
+                  {new Date(video.date_end).toLocaleString()}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </PageContainer>
   );
 };
